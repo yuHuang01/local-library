4079492eb21bc3591d17540949e4e28f4771f15e
yuHuang01 <huang.y0112@gmail.com>
2021-03-28 17:57:38 +0200

    Wrote the basic request handling functions for each model controller (only gave them name all of them are only responding with re.send())

---
+++ controllers/authorController.js
@@ -0,0 +1,42 @@
+const Author = require('../models/author');
+
+//Display a list of all author
+exports.author_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author list');
+}
+
+//Display detail page for a specific author
+exports.auhtor_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author detail:' + req.params.id);
+}
+
+//Display author create form on GET 
+exports.author_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create POST');
+}
+
+//Handle author create on POST
+exports.author_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create POST');
+}
+
+//Display Author delete form on GET
+exports.author_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete GET');
+}
+
+//Handle author delete on POST 
+exports.author_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete POST');
+}
+
+//Display auhtor update form on GET 
+exports.author_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update GET');
+} 
+
+
+//Handle author update on POST 
+exports.author_update_POST = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update POST');
+}
