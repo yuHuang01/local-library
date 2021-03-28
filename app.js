ac1c7ca1d606c64aaab042762322add44afe3a10
yuHuang01 <huang.y0112@gmail.com>
2021-03-28 18:16:04 +0200

    Modified the get method in index.js. And set the route for catalog in app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongodbUrl = require('./mongodb');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
+
 
 var app = express();
 
@@ -28,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/cool.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cool = express.Router();
-
-cool.get('/', (req, res) => {
-  return res.send('You are so cool')
-})
-
-module.exports = cool;
