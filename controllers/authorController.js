b4a4c584d4e97df8e65d85f199dfe4a055ca258d
yuHuang01 <huang.y0112@gmail.com>
2021-03-29 15:17:31 +0200

    Created author_list.pug, modified authorController. Installed luxon to format the date times back to normal. And  added two new virtual for AuthorSchema in author.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,8 +1,15 @@
+const { nextTick } = require('async');
 const Author = require('../models/author');
 
 //Display a list of all author
-exports.author_list = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author list');
+exports.author_list = (req, res, next) => {
+  Author.find()
+    .sort([['family_name', 'ascending']])
+    .exec((err, list_authors) => {
+      if(err) return next(err);
+
+      res.render('author_list', {title: 'Author List', author_list: list_authors});
+    })
 }
 
 //Display detail page for a specific author
--- models/author.js
+++ models/author.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const { DateTime } = require('luxon');
+
 const AuthorSchema = new Schema({
   first_name: {
     type: String,
@@ -33,6 +35,14 @@ AuthorSchema.virtual('lifespan').get(function() {
 //-url
 AuthorSchema.virtual('url').get(function() {
   return '/catalog/author/' + this._id;
+})
+//-date of birth back formatted
+AuthorSchema.virtual('date_of_birth_back').get(function() {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+})
+//-date of death back formatted
+AuthorSchema.virtual('date_of_death_back').get(function() {
+  return (this.date_of_death === undefined)? undefined : DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
 })
 
 module.exports = mongoose.model('Author', AuthorSchema)
